1650bec2732da123a2b189cb00cbb031ca7bcfd6
root <root@instance-1.c.linux-classs.internal>
2019-05-02 01:21:37 +0000

    gcp cluster create

--- k8-main.tf
+++ k8-main.tf
@@ -1,33 +1,27 @@
 resource "google_container_cluster" "gcpcluster" {
+  project    = "linux-classs"
   name       = "gcpcluster"
   network    = "default"
   subnetwork = "default"
   zone       = "us-central1-a"
 
-  #  additional_zones     = ""
+  # additional_zones = "${var.additional_zones}"
 
   master_auth {
-    username = "cluster"
-    password = "Cps50364srym7645747"
+    username = "admin"
+    password = "StrongP1~assworafasfweaqg32d"
   }
   initial_node_count = "2"
   node_config {
-    machine_type = "defaults"
-
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/compute",
-      "https://www.googleapis.com/auth/devstorage.read_only",
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring",
-    ]
+    # machine_type = ""
+    # oauth_scopes = [  #   "https://www.googleapis.com/auth/compute",  #   "https://www.googleapis.com/auth/devstorage.read_only",  #   "https://www.googleapis.com/auth/logging.write",  #   "https://www.googleapis.com/auth/monitoring",  # ]
   }
 }
 
 # resource "random_string" "password" {
-#     length  = 16
-#     special = true
-#     number  = true
-#     lower   = true
-#     upper   = true
+#   length  = 16
+#   special = true
+#   number  = true
+#   lower   = true
+#   upper   = true
 # }
-
