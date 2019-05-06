f5856bc8389a1e830137dcc4351bab52901669a3
Dilfuza Ibaidullaeva <47833854+dilfuza97@users.noreply.github.com>
2019-05-06 18:45:30 -0500

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node{
   stage("Pulling Repo"){
-    git 'git@github.com:dilfuza97/Terraform-aws-K8_modules.git'
+    git 'https://github.com/dilfuza97/GCP_K8_modules.git'
   }
 
   stage("Terraform"){
